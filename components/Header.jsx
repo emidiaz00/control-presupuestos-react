c43aa98359620b8b3b933634933d1eeb21a18ab8
emidiaz00 <emildsn00@gmail.com>
2022-05-09 19:20:38 -0300

    agrego nuevo componente de Mensaje, validación componente de presupuesto

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 
-const Header = ({presupuesto, setPresupuesto}) => {
+const Header = ({
+  presupuesto, 
+  setPresupuesto, 
+  isValidPresupuesto, 
+  setIsValidPresupuesto
+}) => {
   return (
     <header>
         <h1>Planificador de gastos</h1>
@@ -9,7 +14,8 @@ const Header = ({presupuesto, setPresupuesto}) => {
         <NuevoPresupuesto 
          presupuesto={presupuesto}
          setPresupuesto={setPresupuesto}
-        />
+         setIsValidPresupuesto={setIsValidPresupuesto}
+         />
     </header>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import Header from '../components/header'
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   
   return (
    <div>
      <Header
      presupuesto={presupuesto}
      setPresupuesto={setPresupuesto}
+     isValidPresupuesto={isValidPresupuesto}
+     setisValidPresupuesto={setIsValidPresupuesto}
      />
      
    </div>
