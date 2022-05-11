4dd41d12fd1f7d51e62ae5e22ce10d631f207bf9
emidiaz00 <emildsn00@gmail.com>
2022-05-11 02:25:33 -0300

    agrego useState para mostrar modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,46 @@ import { useState } from 'react'
 import Header from '../components/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
-
+  
 
 function App() {
-
+  
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
+  const [modal, setModal] = useState(false)
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+  }
   
   return (
-   <div>
-     <Header
-     presupuesto={presupuesto}
-     setPresupuesto={setPresupuesto}
-     isValidPresupuesto={isValidPresupuesto}
-     setIsValidPresupuesto={setIsValidPresupuesto}
-     />
-
+    <div>
+    <Header
+    presupuesto={presupuesto}
+    setPresupuesto={setPresupuesto}
+    isValidPresupuesto={isValidPresupuesto}
+    setIsValidPresupuesto={setIsValidPresupuesto}
+    />
     
-    {isValidPresupuesto && (
-       <div className='nuevo-gasto'>
-       <img src={IconoNuevoGasto} alt="Icono Nuevo Gasto" />
-     
-     </div>
-     )}
-     
-    </div>
-  )
-}
+    {/* Cuando isValidPresupuesto sea TRUE va ejecutar lo siguiente */}
+        {isValidPresupuesto && (
+      <div className='nuevo-gasto'>
+      <img 
+      src={IconoNuevoGasto} 
+      alt="Icono Nuevo Gasto"
+      onClick={handleNuevoGasto}
+      />
+      
+      
+      </div>
+      )}
 
-export default App
+     
+      
+      </div>
+      )
+    }
+    
+    export default App
+    
